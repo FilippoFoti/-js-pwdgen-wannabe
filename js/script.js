5e844f54b40eac484402782dc586f0df58063c31
FilippoFoti <filippofoti7@gmail.com>
2023-03-07 16:01:58 +0100

    prova

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Creazione Password insicurissima
+alert('ATTENZIONE! Questo è il sito migliore del mondo per creare una password inutile :)');
+
+// Chiedere il nome all'utente
+const username = prompt("Ciao utente, qual è il tuo nome?");
+console.log(username);
+
+// Chiedere il cognome all'utente
+const surname = prompt("Qual è il tuo cognome?");
+console.log(surname);
+
+// Chiedere il colore preferito all'utente
+const favoriteColor = prompt("Qual è il tuo colore preferito?");
+console.log(favoriteColor);
+
+// Generiamo la nuova password
+const password = `Utente, questa è la tua nuova inutile password: ${username + surname + favoriteColor + 21}`;
+console.log(password);
+
+// Stampiamo la nuova inutile password
+document.getElementById('password-result').innerHTML = password;
